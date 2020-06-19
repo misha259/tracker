26d661e13293ad590eb5d7ce06382fdd63156b3e
Миша) <mishaleonov292@gmail.com>
2020-06-20 02:08:58 +0300

    завершил влегкую

--- interface.py
+++ interface.py
@@ -5,15 +5,30 @@ okno.geometry("300x500")
 okno.resizable(width=False, height=False)
 okno.title("Финансовый калькулятор")
 
+
+def add(button):
+    main.add_money(int(eSumma.get()), eKomm.get())
+    lBalans['text'] = main.balance()
+    eSumma.delete(0, END)
+    eKomm.delete(0, END)
+
+
+def remove(button):
+    main.subtrack_money(int(eSumma.get()), eKomm.get())
+    lBalans['text'] = main.balance()
+    eSumma.delete(0, END)
+    eKomm.delete(0, END)
+
+
 summ='Введите сумму'
 komm='По желанию, оставьте комментарий'
 lbl=Label(okno, text='Ваш баланс:')
 bRashod=Button(okno, text='расход')
 bDohod=Button(okno, text='доход')
 bGrafic=Button(okno, text='график')
-lBalans=Label(okno, textvariable=main.balance(), font=("Tahoma", 50))
-eSumma=Entry(okno,textvariable=summ)
-eKomm=Entry(okno,textvariable=komm)
+lBalans=Label(okno, text=main.balance(), font=("Tahoma", 50))
+eSumma=Entry(okno,text=summ)
+eKomm=Entry(okno,text=komm)
 
 lbl.place(x=60,y=5,width=100,height=27)
 lBalans.place(y=37,x=20,width=260, height=80)
@@ -23,26 +38,9 @@ bDohod.place(x=10,y=347,width=135, height=40)
 bRashod.place(x=155,y=347,width=135, height=40)
 bGrafic.place(x=100,y=417,width=100, height=50)
 
-
-
-
-
-# lBalans.grid(row = 1, column = 1,rowspan = 2, columnspan = 2)
-# eSumma.grid(row = 3, column = 1, columnspan = 2)
-# eKomm.grid(row = 4, column = 1, columnspan = 2)
-# bDohod.grid(row = 5, column = 1)
-# bRashod.grid(row = 5, column = 2)
-# bGrafic.grid(row = 7, column = 1, columnspan = 2)
-
-
-
-
-
-
-
-
-
-
+bDohod.bind("<Button-1>", add)
+bRashod.bind("<Button-1>", remove)
+bGrafic.bind("<Button-1>", main.grafic)
 
 
 
